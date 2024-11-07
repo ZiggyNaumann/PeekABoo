5d14375b745df212bcf841d164ebb4cc00bc58e3
Nils Rietveld <its.just.nils@gmail.com>
2024-11-07 10:35:20 +0100

    wip on setting up interaction flow

---
+++ Assets/PeekABoo/Scripts/Characters/Players/PlayerCharacterInteracting.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using PeekABoo.Interacting;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace PeekABoo.Characters.Players
+{
+    public class PlayerCharacterInteracting : PlayerCharacterComponent
+    {
+        [SerializeField] private float spherecastRadius = 0.1f;
+        [SerializeField] private float spherecastDistance = 5f;
+
+        private PlayerCharacterInput playerCharacterInput;
+        private Interactable targetedInteractable;
+
+        protected override void OnInjected()
+        {
+            base.OnInjected();
+
+            playerCharacterInput = Owner.GetCharacterComponent<PlayerCharacterInput>();
+        }
+
+        private void Update()
+        {
+            // Spherecast for interactable objects
+            RaycastHit[] hits = Physics.SphereCastAll(Owner.FirstPersonCamera.transform.position, spherecastRadius,
+                                                      Owner.FirstPersonCamera.transform.forward, spherecastDistance);
+
+            Interactable nearestInteractable = null;
+
+            foreach (RaycastHit hit in hits)
+            {
+                if (!hit.collider.TryGetComponent(out Interactable interactable))
+                {
+                    continue;
+                }
+
+                if (nearestInteractable == null)
+                {
+                    nearestInteractable = interactable;
+                }
+                else
+                {
+                    float currentDistance = Vector3.Distance(Owner.FirstPersonCamera.transform.position, interactable.transform.position);
+                    float nearestDistance = Vector3.Distance(Owner.FirstPersonCamera.transform.position, nearestInteractable.transform.position);
+
+                    if (currentDistance < nearestDistance)
+                    {
+                        nearestInteractable = interactable;
+                    }
+                }
+            }
+
+            if (nearestInteractable != null)
+            {
+                if (targetedInteractable == null)
+                {
+                    targetedInteractable = nearestInteractable;
+                    targetedInteractable.ShowHighlight();
+                }
+                else if (targetedInteractable != nearestInteractable)
+                {
+                    targetedInteractable.HideHighlight();
+                    targetedInteractable = nearestInteractable;
+                    targetedInteractable.ShowHighlight();
+                }
+            }
+            else
+            {
+                if (targetedInteractable != null)
+                {
+                    targetedInteractable.HideHighlight();
+                    targetedInteractable = null;
+                }
+            }
+        }
+    }
+}
---
+++ Assets/PeekABoo/Scripts/Interacting/Interactable.cs
@@ -0,0 +1,29 @@
+﻿using CardboardCore.DI;
+
+namespace PeekABoo.Interacting
+{
+    public class Interactable : CardboardCoreBehaviour
+    {
+        [Inject] private InteractableRegistry interactableRegistry;
+
+        protected override void OnInjected()
+        {
+            interactableRegistry.RegisterInteractable(this);
+        }
+
+        protected override void OnReleased()
+        {
+            interactableRegistry.UnregisterInteractable(this);
+        }
+
+        public void ShowHighlight()
+        {
+            // Show outline
+        }
+
+        public void HideHighlight()
+        {
+            // Hide outline
+        }
+    }
+}
